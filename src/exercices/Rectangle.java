f079356846f80f64aab27b1afd0159f19c510ca3
Noël Plouzeau <noel.plouzeau@irisa.fr>
2017-02-07 16:17:03 +0100

    Correction des exercices du 2017-02-07 mardi après-midi

---
+++ src/exercices/Rectangle.java
@@ -0,0 +1,37 @@
+package exercices;
+
+public class Rectangle {
+	private double largeur;
+	private double hauteur;
+
+	public double getLargeur() {
+		return largeur;
+	}
+	
+	public double getHauteur() {
+		return hauteur;
+	}
+	
+	public void setLargeur(double l) {
+		if(l <= 0) {
+			throw new IllegalArgumentException();
+		}
+		largeur = l;
+	}
+	
+	public void setHauteur(double h) {
+		if(h <= 0) {
+			throw new IllegalArgumentException();
+		}
+		hauteur = h;
+	}
+	
+	public double surface() {
+		return largeur * hauteur;
+	}
+	
+	public void quadrupler() {
+		largeur = 2 * largeur;
+		hauteur = 2 * hauteur;
+	}
+}
---
+++ src/exercices/RectangleTest.java
@@ -0,0 +1,39 @@
+package exercices;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RectangleTest {
+
+	@Test
+	public void testSetLargeur() {
+		Rectangle rect1 = new Rectangle();
+		
+		rect1.setLargeur(10.0);
+		
+		assertEquals(10.0,rect1.getLargeur(),10e-20);
+	}
+	
+	@Test
+	public void testSurface() {
+		Rectangle rect1 = new Rectangle();
+		
+		rect1.setLargeur(10.0);
+		rect1.setHauteur(20.0);
+		
+		assertEquals(200.0, rect1.surface(), 10e-20);
+	}
+	
+	@Test
+	public void testQuadrupler() {
+		Rectangle rect1 = new Rectangle();
+		
+		rect1.setLargeur(10.0);
+		rect1.setHauteur(20.0);
+		rect1.quadrupler();
+		
+		assertEquals(800.0, rect1.surface(), 10e-20);
+	}
+
+}
