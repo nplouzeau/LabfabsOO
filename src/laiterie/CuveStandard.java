d6556813345d9e048bd005eddb978b716d0fe0ef
Noël Plouzeau <noel.plouzeau@irisa.fr>
2017-02-14 11:57:18 +0100

    Ajout des opérations vider et inscrire

--- src/laiterie/CuveStandard.java
+++ src/laiterie/CuveStandard.java
@@ -1,13 +1,34 @@
 package laiterie;
 
-public class CuveStandard implements Cuve {
+import java.util.Collection;
 
+public class CuveStandard implements Cuve {
+	private Collection<ObservateurCuve> observateurs;
 	private final int capacite = 100000;
 	private int niveau;
 
+	/**
+	 * Ajoute le volume dans la cuve.
+	 * @param volume le à ajouter dans la cuve
+	 * @throws IllegalArgumentException si volume <= 0
+	 */
 	@Override
 	public void remplir(int volume) {
+		if(volume <= 0) {
+			throw new IllegalArgumentException();
+		}
 		niveau = niveau + volume;
+	}
+
+	@Override
+	public void vider(int volume) {
+		niveau = niveau - volume;
+	}
+
+	@Override
+	public void inscrire(ObservateurCuve obs) {
+		observateurs.add(obs);
+
 	}
 
 }
